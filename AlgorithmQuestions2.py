2f8d07be5c5c9ca38142b67fb287718599ada220
Olivia Onu <128523943+oliviao123@users.noreply.github.com>
2023-06-01 20:39:59 +0100

    Update AlgorithmQuestions2.py

--- AlgorithmQuestions2.py
+++ AlgorithmQuestions2.py
@@ -17,3 +17,6 @@ print(isMissing([4,5,-1,3, 5]))  # THROW ERROR Invalid input, non-numeric value 
 print(isMissing([ 1, 3, 7, 5, 6, 2 ])) #  THROW ERROR Invalid input, non-numeric value detected
 
 
+# The code is efficient and easy to understand. It calculates the missing number in an unsorted array by leveraging the mathematical property of the sum of consecutive numbers. The time complexity is O(n), and the space complexity is O(1).
+
+# An alternative approach could involve using a hash set to check the presence of numbers in the given range. By iterating over the array and adding each number to the set, we can identify the missing number. However, this approach has a higher space complexity of O(n) because it requires storing the numbers in the set.
